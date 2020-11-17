979c7ce449168cdca1f0b5710c0a361393dac3dd
jedtinio123 <70248047+jedtinio123@users.noreply.github.com>
2020-11-17 19:58:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Starter Template · Bootstrap</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+      <!-- Bootstrap core CSS -->
+    <link href="/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#563d7c">
+
+
+    <style>
+      html,
+      body{
+        background-image: url(bg.png);
+        background-size: cover;
+        background-position: fixed;
+        background-repeat: no-repeat;
+        min-height: 100%;
+      }
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+      <a class="navbar-brand" href="#">Jed Tinio</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Profile <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="output.html">Outputs</a>
+          </li>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  <div class="container" style="padding-top: 100px;margin: auto auto; text-align: center; padding-bottom: 100px">
+    <div class="starter-template">
+      <img src="dp.jpg" width="200px" style="border-radius: 200px"/>
+    </div>
+    <div class="row" style="text-align:center; background-color: rgba(255, 255, 255, 0.938);
+    border-radius: 50px; margin-top: -100px; padding-top: 100px;">
+      <div class="col" style="text-align: center;padding-right: 50px">
+        <img src="gamers.png" style="border-radius: 50px; margin-top: 10px" width="425px" />
+      </div>
+      <div class="col" style="text-align: left;padding-right: 50px;margin-top: 50px;">
+        <p><b>Hello, I'm Jed.</b></br>My Full-name is Jed Tristan S. Tinio and I'm already 20-years old. 
+          You may call me Jed, but in the gaming community they call me Rem.</br></br>
+          I have met different kinds of people, from different city or country because of Social Media so I guess social media is also part of my life.</p>
+      </div>
+    </div>
+    <div class="row" style="text-align:center; background-color: rgba(255, 255, 255, 0.938);height: 500px;
+    border-radius: 50px; margin-top: -100px; padding-top: 100px;">
+      <div class="col" style="text-align: left;padding-left: 50px; margin-top: 50px">
+        <p>I'm a competitive person and loves playing online games, watching anime or reading manga. 
+          Also, I love korean pop culture or which is also known as K-Pop and I stan the group Blackpink and Twice. 
+        </br></br>Another fun fact is that I have collection of blackpink merchandises which I'll soon be displaying in my own room. 
+        When it comes to academics, I'm a chill person but I love being challenged and also I'm grade conscious 
+        because of my parents that's why I am studying to achieve honors.</p>
+      </div>
+      <div class="col" style="text-align: center;padding-right: 50px;margin-top: 100px;">
+        <img src="collection.jpg" style="border-radius: 50px; margin-top: -50px" width="425px" />
+      </div>
+    </div>
+  </div>
+</body>
+
+</main><!-- /.container -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script></body>
+</html>
